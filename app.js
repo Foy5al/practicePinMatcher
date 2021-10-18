0c8d754f716f1be9ee6294185772132d420e366f
Mohammad Foysal <mh.foysal.h@gmail.com>
2021-10-19 03:23:38 +0600

    3rd commit

--- app.js
+++ app.js
@@ -15,3 +15,33 @@ function generatePin() {
     document.getElementById('pin-display').value = pin;
 };
 
+document.getElementById('calc-number').addEventListener('click', function (event) {
+    const clickednumber = event.target.innerText;
+    const displayNumber = document.getElementById('display-calc-num');
+    if (isNaN(clickednumber)) {
+        if (clickednumber == 'C') {
+            displayNumber.value = '';
+        }
+    }
+    else {
+        const previousNumber = displayNumber.value;
+        const newNumber = previousNumber + clickednumber;
+        displayNumber.value = newNumber;
+    }
+});
+
+function verifyPin() {
+    const displayPin = document.getElementById('pin-display').value;
+    const inputPin = document.getElementById('display-calc-num').value;
+    if (displayPin == inputPin) {
+        const successMsg = document.getElementById('success');
+        successMsg.style.display = 'block';
+        document.getElementById('fail').style.display = 'none';
+    }
+    else {
+        const successMsg = document.getElementById('success');
+        successMsg.style.display = 'none';
+        document.getElementById('fail').style.display = 'block';
+    }
+}
+
